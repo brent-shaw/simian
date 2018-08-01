216881a01173dc0f688a5f6e9f088362b095edbf
brent-shaw <shaw@live.co.za>
2018-08-01 22:08:10 +0200

    just a start

---
+++ industrialobjects.py
@@ -0,0 +1,132 @@
+import random
+from colourterm import tform
+
+#==============================================================================
+
+class industial_object():
+    def __init__(self, v=0, l=0, t=0, d=1):
+        #print("Initialised industial_object")
+        self.capacity = v
+        self.value = v
+        self.limit = l
+        self.trend = t
+        self.default = d
+        self.fault = False
+
+    def setValue(self, v):
+        self.value = v
+        if (self.trend == 0):
+            if (self.value <= self.limit):
+                self.fault = True
+        else:
+            if (self.value >= self.limit):
+                self.fault = True
+
+    def inc(self, v):
+        self.value += v
+        if (self.trend == 0):
+            if (self.value <= self.limit):
+                self.fault = True
+        else:
+            if (self.value >= self.limit):
+                self.fault = True
+
+    def dec(self, v):
+        self.value -= v
+        if (self.trend == 0):
+            if (self.value <= self.limit):
+                self.fault = True
+        else:
+            if (self.value >= self.limit):
+                self.fault = True
+
+    def update(self):
+        if (self.trend == 0):
+            self.dec(self.default+(random.randint(0,10)/10))
+        else:
+            self.inc(self.default+(random.randint(0,10)/10))
+
+#==============================================================================
+
+class industrial_sensor():
+
+    def __init__(self, o):
+        self.source = o
+        #print("Initialised sensor")
+
+    def read(self):
+        return self.source.value
+
+
+#==============================================================================
+
+class actuator():
+
+    def __init__(self):
+        print("Initialised actuator")
+
+#==============================================================================
+
+class rtu():
+
+    def __init__(self):
+        print("Initialised rtu")
+
+#==============================================================================
+
+class plc():
+
+    def __init__(self):
+        print("Initialised plc")
+
+#==============================================================================
+
+class control_system():
+    def __init__(self):
+        self.objects = []
+        self.sensors = []
+        self.actuators = []
+        self.plcs = []
+        self.rtus = []
+        #print("Initialised control_system")
+
+    def addObject(self, o):
+        self.objects.append(o)
+
+    def addSensor(self, o):
+        self.sensors.append(o)
+
+    def addPLC(self, o):
+        self.plcs.append(o)
+
+    def update(self):
+        update = ["SYSTEM UPDATE"]
+        updated = False
+        formatter = "{: <15}"
+        for sensor in self.sensors:
+            sensor.source.update()
+            if (sensor.read() < (sensor.source.capacity*0.3)):
+                sensor.source.inc((random.randint(0,100)+(random.randint(0,100))))
+                update.append("> "+tform("LEVEL: Adding water",'OKBLUE')+(" "*4))
+                updated = True
+            else:
+                update.append(">")
+            formatter += " {: <25}"
+        return (updated, formatter.format(*update))
+
+
+    def reportTanks(self, cycle):
+        if cycle == None:
+            output = ["END CONDITIONS"]
+        else:
+            output = ["Cycle: "+str(cycle)]
+        for o in self.objects:
+            if (o.fault):
+                output.append("> "+tform("FAULT: Tank is dry",'FAIL')+(" "*8))
+            else:
+                if o.value <= (o.capacity*0.35):
+                    output.append("> "+tform("tank level: {0:.2f}".format(o.value),'WARNING')+(" "*5))
+                else:
+                    output.append("> tank level: {0:.2f}".format(o.value))
+        return "{: <15} {: <25} {: <25} {: <25} {: <25} {: <25}".format(*output)
+
---
+++ sim.py
@@ -0,0 +1,128 @@
+import decimal
+import json
+import random
+import threading
+from time import sleep
+import sys
+from colourterm import tform
+from industrialobjects import *
+
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+
+#------------------------------------------------------------------------------
+
+temp = 30.0
+
+#==============================================================================
+
+def get_config(filename):
+    try:
+        with open(filename, 'r') as infile:
+            return json.load(infile)
+    except:
+        print("\r"+tform("Incorrect configuration file.", "FAIL"))
+        print("System configuration must be JSON format")
+        sys.exit()
+
+#------------------------------------------------------------------------------
+
+def store_config(filename, config):
+    with open(filename, 'w') as outfile:
+        outfile.write(config)
+
+#------------------------------------------------------------------------------
+
+def store_log(filename, log):
+    with open(filename, 'w') as outfile:
+        for line in log:
+            outfile.write(line+'\n')
+
+#------------------------------------------------------------------------------
+
+def startup(config):
+    print(tform("Starting program",'HEADER'))
+
+    print("Fetching configuration")
+    sysconf = get_config(config)
+    print(json.dumps(sysconf, indent=2, sort_keys=True))
+
+    print(tform("Program running",'OKGREEN'))
+
+    return sysconf
+
+#------------------------------------------------------------------------------
+
+def shutdown(logs):
+    print(tform("Halting program",'WARNING'))
+
+    # for line in logs:
+    #     print(line)
+
+    store_log('tank_sim.log', logs)
+
+    print(tform("Program ended", 'OKGREEN'))
+
+#------------------------------------------------------------------------------
+
+def noFaults(a):
+    for o in a:
+        if (o.fault):
+            return False
+    return True
+
+#------------------------------------------------------------------------------
+
+def runSimulation(system, cycles):
+    cycleCount = 0
+    while(noFaults(system.objects)):
+        updates = system.update()
+
+        report = system.reportTanks(cycleCount)
+        if updates[0]:
+            yield updates[1]
+        yield report
+
+        cycleCount += 1
+        #sleep(0.01)
+        if cycleCount == cycles:
+            break
+
+    report = system.reportTanks(None)
+    yield report
+
+def main():
+    if(len(sys.argv) < 3):
+        print("\r"+tform("Incorrect usage.", "FAIL"))
+        print("eg: sim.py <system_configuration> <simulated_cycles>")
+        sys.exit()
+
+    config = str(sys.argv[1])
+    cycles = int(sys.argv[2])
+
+    startup(config)
+
+    system = control_system()
+
+    system.addObject(industial_object(1000))
+    system.addObject(industial_object(1000))
+    system.addObject(industial_object(1000))
+    system.addObject(industial_object(1000))
+    system.addObject(industial_object(1000))
+
+    for obj in system.objects:
+        system.addSensor(industrial_sensor(obj))
+
+    try:
+        print("Running simulation for "+str(cycles)+" cycles.")
+        logs = runSimulation(system, cycles)
+        print(tform("Simulation completed successfully", 'OKGREEN'))
+    except(KeyboardInterrupt):
+        print("\r"+tform("Simultaion interrupted by user", "FAIL"))
+
+    shutdown(logs)
+
+#------------------------------------------------------------------------------
+
+main()
+
+#------------------------------------------------------------------------------
